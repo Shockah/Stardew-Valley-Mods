d0a2c1d0a1d47b398f7536aedcc6be2964372fd7
Shockah <me@shockah.pl>
2022-02-20 22:15:31 +0100

    _Common: Common utilities for the other mods

---
+++ _Common/TranspileHelper.cs
@@ -0,0 +1,146 @@
+﻿using HarmonyLib;
+using System.Reflection.Emit;
+
+namespace Shockah.CommonModCode
+{
+	public static class TranspileHelper
+	{
+		public static bool IsLdcI4(this CodeInstruction instruction)
+		{
+			return instruction.opcode == OpCodes.Ldc_I4 ||
+				instruction.opcode == OpCodes.Ldc_I4_S ||
+				instruction.opcode == OpCodes.Ldc_I4_0 ||
+				instruction.opcode == OpCodes.Ldc_I4_1 ||
+				instruction.opcode == OpCodes.Ldc_I4_2 ||
+				instruction.opcode == OpCodes.Ldc_I4_3 ||
+				instruction.opcode == OpCodes.Ldc_I4_4 ||
+				instruction.opcode == OpCodes.Ldc_I4_5 ||
+				instruction.opcode == OpCodes.Ldc_I4_6 ||
+				instruction.opcode == OpCodes.Ldc_I4_7 ||
+				instruction.opcode == OpCodes.Ldc_I4_8 ||
+				instruction.opcode == OpCodes.Ldc_I4_M1;
+		}
+
+		public static bool IsLdcI4(this CodeInstruction instruction, int value)
+		{
+			switch (value)
+			{
+				case 0:
+					if (instruction.opcode == OpCodes.Ldc_I4_0)
+						return true;
+					break;
+				case 1:
+					if (instruction.opcode == OpCodes.Ldc_I4_1)
+						return true;
+					break;
+				case 2:
+					if (instruction.opcode == OpCodes.Ldc_I4_2)
+						return true;
+					break;
+				case 3:
+					if (instruction.opcode == OpCodes.Ldc_I4_3)
+						return true;
+					break;
+				case 4:
+					if (instruction.opcode == OpCodes.Ldc_I4_4)
+						return true;
+					break;
+				case 5:
+					if (instruction.opcode == OpCodes.Ldc_I4_5)
+						return true;
+					break;
+				case 6:
+					if (instruction.opcode == OpCodes.Ldc_I4_6)
+						return true;
+					break;
+				case 7:
+					if (instruction.opcode == OpCodes.Ldc_I4_7)
+						return true;
+					break;
+				case 8:
+					if (instruction.opcode == OpCodes.Ldc_I4_8)
+						return true;
+					break;
+				case -1:
+					if (instruction.opcode == OpCodes.Ldc_I4_M1)
+						return true;
+					break;
+			}
+			return (instruction.opcode == OpCodes.Ldc_I4 && (int)instruction.operand == value) ||
+				(value < 256 && instruction.opcode == OpCodes.Ldc_I4_S && (sbyte)instruction.operand == value);
+		}
+
+		public static int? GetLdcI4Value(this CodeInstruction instruction)
+		{
+			if (instruction.opcode == OpCodes.Ldc_I4_0)
+				return 0;
+			else if (instruction.opcode == OpCodes.Ldc_I4_1)
+				return 1;
+			else if (instruction.opcode == OpCodes.Ldc_I4_2)
+				return 2;
+			else if (instruction.opcode == OpCodes.Ldc_I4_3)
+				return 3;
+			else if (instruction.opcode == OpCodes.Ldc_I4_4)
+				return 4;
+			else if (instruction.opcode == OpCodes.Ldc_I4_5)
+				return 5;
+			else if (instruction.opcode == OpCodes.Ldc_I4_6)
+				return 6;
+			else if (instruction.opcode == OpCodes.Ldc_I4_7)
+				return 7;
+			else if (instruction.opcode == OpCodes.Ldc_I4_8)
+				return 8;
+			else if (instruction.opcode == OpCodes.Ldc_I4_M1)
+				return -1;
+			else if (instruction.opcode == OpCodes.Ldc_I4)
+				return (int)instruction.operand;
+			else if (instruction.opcode == OpCodes.Ldc_I4_S)
+				return (sbyte)instruction.operand;
+			else
+				return null;
+		}
+
+		public static int? GetLdargIndex(this CodeInstruction instruction)
+		{
+			if (instruction.opcode == OpCodes.Ldarg_0)
+				return 0;
+			else if (instruction.opcode == OpCodes.Ldarg_1)
+				return 1;
+			else if (instruction.opcode == OpCodes.Ldarg_2)
+				return 2;
+			else if (instruction.opcode == OpCodes.Ldarg_3)
+				return 3;
+			else if (instruction.opcode == OpCodes.Ldarg)
+				return (int)instruction.operand;
+			else if (instruction.opcode == OpCodes.Ldarg_S)
+				return (sbyte)instruction.operand;
+			else
+				return null;
+		}
+
+		public static bool IsBneUn(this CodeInstruction instruction)
+		{
+			return instruction.opcode == OpCodes.Bne_Un || instruction.opcode == OpCodes.Bne_Un_S;
+		}
+
+		public static bool IsBge(this CodeInstruction instruction)
+		{
+			return instruction.opcode == OpCodes.Bge || instruction.opcode == OpCodes.Bge_S;
+		}
+
+		public static bool IsBgeUn(this CodeInstruction instruction)
+		{
+			return instruction.opcode == OpCodes.Bge_Un || instruction.opcode == OpCodes.Bge_Un_S;
+		}
+
+		public static bool IsBlt(this CodeInstruction instruction)
+		{
+			return instruction.opcode == OpCodes.Blt || instruction.opcode == OpCodes.Blt_S;
+		}
+
+		public static bool IsBltUn(this CodeInstruction instruction)
+		{
+			return instruction.opcode == OpCodes.Blt_Un || instruction.opcode == OpCodes.Blt_Un_S;
+		}
+	}
+}
