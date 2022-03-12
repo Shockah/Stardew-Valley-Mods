4434a3566dababf66c299d218e4733f47d9bd0b7
Shockah <me@shockah.pl>
2022-03-12 17:50:19 +0100

    UIKit: General code improvements

--- UIKit/TextAlignment.cs
+++ UIKit/TextAlignment.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Shockah.UIKit
+﻿namespace Shockah.UIKit
 {
 	public enum TextAlignment { Left, Center, Right }
 }
--- UIKit/UILayoutConstraints.cs
+++ UIKit/UILayoutConstraints.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 
 namespace Shockah.UIKit
 {
 	public static class UILayoutConstraints
 	{
+		[Pure]
 		public static UIView? GetStoringView(this UILayoutConstraint self)
 		{
 			if (!self.IsActive)
@@ -24,5 +27,43 @@ namespace Shockah.UIKit
 			foreach (var constraint in constraints)
 				constraint.Deactivate();
 		}
+
+		[Pure]
+		public static IEnumerable<UILayoutConstraint> MakeEqualConstraints<ConstrainableType>(
+			Func<ConstrainableType, UIAnchor> anchor,
+			IEnumerable<ConstrainableType> constrainables,
+			UILayoutConstraintPriority? priority = null
+		)
+			where ConstrainableType: IConstrainable
+		{
+			var enumerator = constrainables.GetEnumerator();
+			ConstrainableType? first = default;
+			while (enumerator.MoveNext())
+			{
+				if (first is null)
+				{
+					first = enumerator.Current;
+				}
+				else
+				{
+					yield return new UILayoutConstraint(anchor(enumerator.Current), anchor2: anchor(first), priority: priority);
+				}
+			}
+		}
+
+		[Pure]
+		public static IEnumerable<UILayoutConstraint> MakeEqualConstraints<ConstrainableType>(
+			Func<ConstrainableType, UIAnchor> anchor,
+			UILayoutConstraintPriority priority,
+			params ConstrainableType[] constrainables
+		) where ConstrainableType : IConstrainable
+			=> MakeEqualConstraints(anchor, constrainables, priority);
+
+		[Pure]
+		public static IEnumerable<UILayoutConstraint> MakeEqualConstraints<ConstrainableType>(
+			Func<ConstrainableType, UIAnchor> anchor,
+			params ConstrainableType[] constrainables
+		) where ConstrainableType : IConstrainable
+			=> MakeEqualConstraints(anchor, constrainables, null);
 	}
 }
