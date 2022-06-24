cdbcec36baa5ce7067642c6e885aa40a329a09ee
Shockah <me@shockah.pl>
2022-06-24 11:32:05 +0200

    ProjectFluent: FluentFunction XML docs

--- ProjectFluent/PublicAPI/FluentFunction.cs
+++ ProjectFluent/PublicAPI/FluentFunction.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 
 namespace Shockah.ProjectFluent
 {
+	/// <summary>A delegate implementing a custom function, available to Project Fluent translations.</summary>
+	/// <param name="locale">The locale the translation is being provided for.</param>
+	/// <param name="mod">The mod the translation is being provided for.</param>
+	/// <param name="positionalArguments">A list of positional arguments passed to the function.</param>
+	/// <param name="namedArguments">A list of named arguments passed to the function.</param>
+	/// <returns>The resulting value of the function.</returns>
 	public delegate IFluentFunctionValue FluentFunction(
 		IGameLocale locale,
 		IManifest mod,
