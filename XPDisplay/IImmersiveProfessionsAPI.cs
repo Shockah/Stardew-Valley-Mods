a94eeee07cb2b5f15d538e6b5e81505b7cbc37e9
Shockah <me@shockah.pl>
2022-06-26 21:56:41 +0200

    XPDisplay: Fixed Walk of Life integration

---
+++ XPDisplay/IImmersiveProfessionsAPI.cs
@@ -0,0 +1,14 @@
+﻿namespace Shockah.XPDisplay
+{
+	public interface IImmersiveProfessionsAPI
+	{
+		/// <summary>Get an interface for this mod's config settings.</summary>
+		ModConfig GetConfigs();
+
+		public interface ModConfig
+		{
+			bool EnablePrestige { get; }
+			uint RequiredExpPerExtendedLevel { get; }
+		}
+	}
+}
