513815d40971296d6cb25d11f343d407dbc4b224
Michał Dolaś <me@shockah.pl>
2023-02-19 01:30:51 +0100

    Talented.Vanilla: WIP mod which will actually add vanilla sill talent trees

---
+++ Talented.Vanilla/ITalentedApi.cs
@@ -0,0 +1,92 @@
+﻿using Shockah.Kokoro.Stardew.Skill;
+using Shockah.Kokoro.UI;
+using System;
+using System.Collections.Generic;
+
+namespace Shockah.Talented.Vanilla
+{
+	public interface ITalentedApi
+	{
+		void RegisterTalentTag(ITalentTag tag);
+		void UnregisterTalentTag(ITalentTag tag);
+
+		void RegisterTalent(ITalent talent);
+		void UnregisterTalent(ITalent talent);
+	}
+
+	public interface ITalentTag
+	{
+		string UniqueID { get; }
+		TextureRectangle Icon { get; }
+		string Name { get; }
+		ITalentTag? Parent => null;
+		ISkill? Skill => null;
+	}
+
+	public record BasicTalentTag(
+		string UniqueID,
+		Func<TextureRectangle> IconProvider,
+		Func<string> NameProvider,
+		ITalentTag? Parent = null
+	) : ITalentTag
+	{
+		public TextureRectangle Icon
+			=> IconProvider();
+
+		public string Name
+			=> NameProvider();
+	}
+
+	public record SkillTalentTag(
+		string UniqueID,
+		ISkill Skill
+	) : ITalentTag
+	{
+		public TextureRectangle Icon
+			=> Skill.Icon!;
+
+		public string Name
+			=> Skill.Name;
+	}
+
+	public interface ITalent
+	{
+		string UniqueID { get; }
+		string Name { get; }
+		ITalent? ReplacedTalent { get; }
+		ITalentTag Tag { get; }
+		ITalentRequirements? Requirements { get; }
+		int PointCost => 1;
+
+		bool Matches(ITalentTag tag)
+		{
+			ITalentTag? current = Tag;
+			while (true)
+			{
+				if (current == tag)
+					return true;
+				current = tag.Parent;
+				if (current == null)
+					return false;
+			}
+		}
+	}
+
+	public record Talent(
+		string UniqueID,
+		Func<string> NameProvider,
+		ITalent? ReplacedTalent,
+		ITalentTag Tag,
+		ITalentRequirements? Requirements,
+		int PointCost = 1
+	) : ITalent
+	{
+		public string Name
+			=> NameProvider();
+	}
+
+	public interface ITalentRequirements
+	{
+		bool AreSatisifed(IEnumerable<ITalent> talents);
+	}
+}
---
+++ Talented.Vanilla/TalentedVanilla.cs
@@ -0,0 +1,68 @@
+﻿using Shockah.Kokoro;
+using Shockah.Kokoro.Stardew.Skill;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace Shockah.Talented.Vanilla
+{
+	public class TalentedVanilla : BaseMod
+	{
+		public override void Entry(IModHelper helper)
+		{
+			helper.Events.GameLoop.GameLaunched += OnGameLaunched;
+		}
+
+		private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
+		{
+			ITalentedApi? api = Helper.ModRegistry.GetApi<ITalentedApi>("Shockah.Talented");
+			if (api is not null)
+				SetupTalents(api);
+		}
+
+		private void SetupTalents(ITalentedApi api)
+		{
+			SetupFarmingTalents(api);
+			SetupMiningTalents(api);
+			SetupForagingTalents(api);
+			SetupFishingTalents(api);
+			SetupCombatTalents(api);
+		}
+
+		private void SetupFarmingTalents(ITalentedApi api)
+		{
+			var mainTag = new SkillTalentTag($"{ModManifest.UniqueID}.Farming", VanillaSkill.Farming);
+			api.RegisterTalentTag(mainTag);
+
+			var cropsTag = new BasicTalentTag($"{mainTag.UniqueID}.Crops", () => new(Game1.objectSpriteSheet, Game1.getSourceRectForStandardTileSheet(Game1.objectSpriteSheet, 472, 16, 16)), () => "Crops", mainTag);
+			api.RegisterTalentTag(cropsTag);
+
+			var animalsTag = new BasicTalentTag($"{mainTag.UniqueID}.Animals", () => new(Game1.objectSpriteSheet, Game1.getSourceRectForStandardTileSheet(Game1.objectSpriteSheet, 184, 16, 16)), () => "Animals", mainTag);
+			api.RegisterTalentTag(animalsTag);
+		}
+
+		private void SetupMiningTalents(ITalentedApi api)
+		{
+			var mainTag = new SkillTalentTag($"{ModManifest.UniqueID}.Mining", VanillaSkill.Mining);
+			api.RegisterTalentTag(mainTag);
+		}
+
+		private void SetupForagingTalents(ITalentedApi api)
+		{
+			var mainTag = new SkillTalentTag($"{ModManifest.UniqueID}.Foraging", VanillaSkill.Foraging);
+			api.RegisterTalentTag(mainTag);
+		}
+
+		private void SetupFishingTalents(ITalentedApi api)
+		{
+			var mainTag = new SkillTalentTag($"{ModManifest.UniqueID}.Fishing", VanillaSkill.Fishing);
+			api.RegisterTalentTag(mainTag);
+		}
+
+		private void SetupCombatTalents(ITalentedApi api)
+		{
+			var mainTag = new SkillTalentTag($"{ModManifest.UniqueID}.Combat", VanillaSkill.Combat);
+			api.RegisterTalentTag(mainTag);
+		}
+	}
+}
