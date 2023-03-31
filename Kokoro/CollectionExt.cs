c37a89597a5ff64d2b11128aabeb4ab7b427965f
Michał Dolaś <me@shockah.pl>
2023-03-31 20:57:36 +0200

    Kokoro: Maps; other utilities

---
+++ Kokoro/CollectionExt.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Shockah.Kokoro
+{
+	public static class CollectionExt
+	{
+		public static void Toggle<T>(this ISet<T> set, T element)
+		{
+			if (set.Contains(element))
+				set.Remove(element);
+			else
+				set.Add(element);
+		}
+	}
+}
---
+++ Kokoro/StructRef.cs
@@ -0,0 +1,34 @@
+﻿namespace Shockah.Kokoro
+{
+	public sealed class StructRef<T> where T : struct
+	{
+		public T Value { get; set; }
+
+		public StructRef(T initialValue)
+		{
+			this.Value = initialValue;
+		}
+
+		public static implicit operator StructRef<T>(T value)
+			=> new(value);
+
+		public static implicit operator T(StructRef<T> value)
+			=> value.Value;
+	}
+
+	public sealed class NullableStructRef<T> where T : struct
+	{
+		public T? Value { get; set; }
+
+		public NullableStructRef(T? initialValue = null)
+		{
+			this.Value = initialValue;
+		}
+
+		public static implicit operator NullableStructRef<T>(T? value)
+			=> new(value);
+
+		public static implicit operator T?(NullableStructRef<T> value)
+			=> value.Value;
+	}
+}
