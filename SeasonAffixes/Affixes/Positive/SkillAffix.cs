deacd495141a4b22571e6c2ab326cadde130fa24
Michał Dolaś <me@shockah.pl>
2023-04-16 18:05:52 +0200

    SeasonAffixes: Skill affix

--- SeasonAffixes/Affixes/Positive/SkillAffix.cs
+++ SeasonAffixes/Affixes/Positive/SkillAffix.cs
@@ -1,5 +1,7 @@
 ﻿using Shockah.Kokoro.Stardew;
 using Shockah.Kokoro.UI;
+using StardewModdingAPI.Events;
+using StardewValley;
 using System;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -13,8 +15,10 @@ namespace Shockah.SeasonAffixes.Affixes.Positive
 		private static string ShortID => "Skill";
 		public override string UniqueID => $"{Mod.ModManifest.UniqueID}.{ShortID}:{Skill.UniqueID}";
 		public override string LocalizedName => Skill.Name;
-		public override string LocalizedDescription => Mod.Helper.Translation.Get($"affix.positive.{ShortID}.description.{(VanillaSkill.GetAllSkills().Contains(Skill) ? "Vanilla" : "SpaceCore")}", new { Skill = Skill.Name });
+		public override string LocalizedDescription => Mod.Helper.Translation.Get($"affix.positive.{ShortID}.description.{(Skill is VanillaSkill ? "Vanilla" : "SpaceCore")}", new { Skill = Skill.Name });
 		public override TextureRectangle Icon => Skill.Icon!; // TODO: placeholder icon
+
+		private int? LastXP = null;
 
 		public SkillAffix(ISkill skill)
 		{
@@ -33,6 +37,82 @@ namespace Shockah.SeasonAffixes.Affixes.Positive
 		public double GetProbabilityWeight(OrdinalSeason season)
 			=> Math.Min(2.0 / Mod.AllAffixes.Values.Count(affix => affix is SkillAffix), 1.0);
 
-		// TODO: Skill implementation
+		public override void OnActivate()
+		{
+			UpdateXP();
+			Mod.Helper.Events.GameLoop.DayStarted += OnDayStarted;
+			Mod.Helper.Events.GameLoop.UpdateTicked += OnUpdateTicked;
+
+			if (Skill is VanillaSkill skill)
+				ModifySkillLevel(Game1.player, skill, 3);
+		}
+
+		public override void OnDeactivate()
+		{
+			if (Skill is VanillaSkill skill)
+				ModifySkillLevel(Game1.player, skill, -3);
+
+			Mod.Helper.Events.GameLoop.UpdateTicked -= OnUpdateTicked;
+			LastXP = null;
+		}
+
+		private void OnDayStarted(object? sender, DayStartedEventArgs e)
+		{
+			if (Skill is not VanillaSkill skill)
+				return;
+			ModifySkillLevel(Game1.player, skill, 3);
+		}
+
+		private void OnUpdateTicked(object? sender, UpdateTickedEventArgs e)
+		{
+			UpdateXP();
+		}
+
+		private static void ModifySkillLevel(Farmer player, VanillaSkill skill, int levels)
+		{
+			switch (skill.SkillIndex)
+			{
+				case Farmer.farmingSkill:
+					player.addedFarmingLevel.Value += levels;
+					break;
+				case Farmer.miningSkill:
+					player.addedMiningLevel.Value += levels;
+					break;
+				case Farmer.foragingSkill:
+					player.addedForagingLevel.Value += levels;
+					break;
+				case Farmer.fishingSkill:
+					player.addedFishingLevel.Value += levels;
+					break;
+				case Farmer.combatSkill:
+					player.addedCombatLevel.Value += levels;
+					break;
+				case Farmer.luckSkill:
+					player.addedLuckLevel.Value += levels;
+					break;
+				default:
+					throw new ArgumentException($"{nameof(skill.SkillIndex)} has an invalid value.");
+			}
+		}
+
+		private void UpdateXP()
+		{
+			// TODO: Skill Rings compatibility - i'm assuming right now they will trigger off each other
+
+			int newXP = Skill.GetXP(Game1.player);
+			if (LastXP is null)
+			{
+				LastXP = newXP;
+				return;
+			}
+
+			int extraXP = newXP - LastXP.Value;
+			if (extraXP > 0)
+			{
+				int bonusXP = (int)Math.Ceiling(extraXP * (Skill is VanillaSkill ? 0.2 : 0.25));
+				Skill.GrantXP(Game1.player, bonusXP);
+				LastXP += extraXP;
+			}
+		}
 	}
 }
