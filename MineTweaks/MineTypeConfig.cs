e67ebdec322f84653242de0052cf8cf97c7e03b5
Michał Dolaś <me@shockah.pl>
2023-05-29 14:20:44 +0200

    MineTweaks: New mod

---
+++ MineTweaks/MineTypeConfig.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Shockah.MineTweaks
+{
+	public sealed class MineTypeConfig
+	{
+		[JsonProperty] public float StoneChanceMultiplier { get; internal set; } = 1f;
+		[JsonProperty] public float MonsterChanceMultiplier { get; internal set; } = 1f;
+		[JsonProperty] public float ItemChanceMultiplier { get; internal set; } = 1f;
+		[JsonProperty] public float GemStoneChanceMultiplier { get; internal set; } = 1f;
+		[JsonProperty] public float MonsterMuskChanceMultiplier { get; internal set; } = 2f;
+	}
+}
---
+++ MineTweaks/ModConfig.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace Shockah.MineTweaks
+{
+	public sealed class ModConfig
+	{
+		[JsonProperty] public MineTypeConfig Mine { get; internal set; } = new();
+		[JsonProperty] public MineTypeConfig SkullCavern { get; internal set; } = new();
+		[JsonProperty] public MineTypeConfig Volcano { get; internal set; } = new();
+	}
+}
