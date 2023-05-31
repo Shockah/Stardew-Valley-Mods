a6e2222dcef10975be5eb8d987e062047fa99c94
Michał Dolaś <me@shockah.pl>
2023-05-31 20:04:13 +0200

    Kokoro: ItemExt.IsSameItem improvements; FarmerExt.ConsumeItem

---
+++ Kokoro/Stardew/FarmerExt.cs
@@ -0,0 +1,79 @@
+﻿using StardewValley;
+using System;
+using System.Linq;
+using SObject = StardewValley.Object;
+
+namespace Shockah.Kokoro.Stardew
+{
+	public static class FarmerExt
+	{
+		public static bool ConsumeItem(this Farmer player, Item item, int? amount = null, bool exactQuality = true)
+		{
+			bool IsRequestedItem(Item? inventoryItem, bool exactQuality)
+			{
+				if (inventoryItem is null)
+					return false;
+				if (!inventoryItem.IsSameItem(item, ignoringAmount: true))
+					return false;
+
+				if (item is SObject @object && inventoryItem is SObject inventoryObject)
+				{
+					if (exactQuality)
+					{
+						if (inventoryObject.Quality != @object.Quality)
+							return false;
+					}
+					else
+					{
+						if (inventoryObject.Quality < @object.Quality)
+							return false;
+					}
+				}
+
+				return true;
+			}
+
+			void RemoveAmount(int index, int amount)
+			{
+				if (amount > player.Items[index].Stack)
+					throw new ArgumentException("Not enough of an item to remove.");
+
+				if (player.Items[index].Stack == amount)
+					player.Items[index] = null;
+				else
+					player.Items[index].Stack -= amount;
+			}
+
+			int amountLeft = amount ?? item.Stack;
+
+			var itemsPossibleToRemove = Enumerable.Range(0, player.Items.Count)
+				.Where(i => IsRequestedItem(player.Items[i], exactQuality))
+				.OrderBy(i => player.Items[i] is SObject inventoryObject ? inventoryObject.Quality : SObject.lowQuality)
+				.ThenBy(i => player.Items[i]!.Stack)
+				.ToList();
+
+			foreach (var i in itemsPossibleToRemove)
+			{
+				if (player.Items[i]!.Stack == amountLeft)
+				{
+					RemoveAmount(i, amountLeft);
+					return true;
+				}
+			}
+
+			if (itemsPossibleToRemove.Sum(i => player.Items[i]!.Stack) < amountLeft)
+				return false;
+
+			foreach (var i in itemsPossibleToRemove)
+			{
+				int amountToRemove = Math.Min(amountLeft, player.Items[i]!.Stack);
+				RemoveAmount(i, amountToRemove);
+				amountLeft -= amountToRemove;
+				if (amountLeft <= 0)
+					return true;
+			}
+
+			throw new InvalidOperationException("Invalid state.");
+		}
+	}
+}
--- Kokoro/Stardew/ItemExt.cs
+++ Kokoro/Stardew/ItemExt.cs
@@ -1,4 +1,5 @@
 ﻿using StardewValley;
+using StardewValley.Objects;
 using System;
 using System.Linq;
 
@@ -6,8 +7,24 @@ namespace Shockah.Kokoro.Stardew
 {
 	public static class ItemExt
 	{
-		public static bool IsSameItem(this Item self, Item other)
-			=> self.CompareTo(other) == 0
-			&& self.modData.Pairs.OrderBy(kvp => kvp.Key).SequenceEqual(other.modData.Pairs.OrderBy(kvp => kvp.Key));
+		public static bool IsSameItem(this Item self, Item other, bool ignoringAmount = false)
+		{
+			int oldAmount = other.Stack;
+			if (ignoringAmount)
+				other.Stack = self.Stack;
+			int compareToResult = self.CompareTo(other);
+			other.Stack = oldAmount;
+
+			if (compareToResult != 0)
+				return false;
+
+			if (self is ColoredObject coloredA && other is ColoredObject coloredB && coloredA.color.Value != coloredB.color.Value)
+				return false;
+
+			if (!self.modData.Pairs.OrderBy(kvp => kvp.Key).SequenceEqual(other.modData.Pairs.OrderBy(kvp => kvp.Key)))
+				return false;
+
+			return true;
+		}
 	}
 }
